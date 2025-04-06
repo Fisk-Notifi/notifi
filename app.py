8058f2deed09b7b76c994b5ebe5a797ee997222b
IfeoluwaShode <josephshode@gmail.com>
2025-04-06 00:09:30 -0500

    This is sign up and login page for notifi. It stores user info to notifi database

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-print("app.py has been imported")
+print("app.py has been imported") #this is just to check if the file is being imported for debugging purposes
 from flask import Flask, flash, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-# from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, LoginManager, login_user, logout_user, current_user, login_required
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, ValidationError
@@ -10,12 +9,13 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 import email_validator
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notifi.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:notifidatabase123@localhost/notifi')
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 db = SQLAlchemy(app)
 bcrypt =Bcrypt(app)
 migrate = Migrate(app, db)
@@ -43,8 +43,7 @@ class SignupForm(FlaskForm):
     last_name = StringField('Last Name', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
 
-    #checks is the email already exists in the database
-# In your validate_email method
+    #checks if the email already exists in the database
     def validate_email(self, email):
         existing_user = User.query.filter_by(email=email.data).first()
         if existing_user:
@@ -69,13 +68,18 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user)
-            return redirect(url_for('successfullogintest'))
+    if form.validate_on_submit():            
+        if not form.email.data.endswith('@my.fisk.edu'):
+            flash('Please use your Fisk email to login', 'danger')
         else:
-            flash('Login unsuccessful. Please check email and password', 'danger')
+            user = User.query.filter_by(email=form.email.data).first()
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('successfullogintest'))
+            else:
+                flash('Login unsuccessful. Please check email and password', 'danger')            
+                return redirect(url_for('index'))
+        
     return render_template('index.html', form=form)
 
 @app.route('/successfullogintest', methods=['GET', 'POST'])
@@ -87,12 +91,15 @@ def successfullogintest():
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Account created successfully! You can now log in.', 'success')
-        return redirect(url_for('index'))
+        if not form.email.data.endswith('@my.fisk.edu'):
+            flash('Please use your Fisk email to sign up', 'danger')
+        else:
+            hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+            user = User(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data, password=hashed_password)
+            db.session.add(user)
+            db.session.commit()
+            flash('Account created successfully! You can now log in.', 'success')
+            return redirect(url_for('index'))
     return render_template('signup.html', form=form)
  
 
